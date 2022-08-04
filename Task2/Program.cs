875ce54e153ca26327c18ada414cb4e8e908f5b4
Renat <rzarifullov@gmail.com>
2022-08-04 16:20:31 +0300

    Task2 added

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,3 +7,48 @@
 // 5 9 2 3
 
 // 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите номер строки");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите номер столбца");
+int m = int.Parse(Console.ReadLine());
+
+int [,] numbers = new int [5,5];
+
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) && m > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(1, 9);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.WriteLine(""); 
+    }
+}
